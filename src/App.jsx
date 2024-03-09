12fae0ffe8802eba5749f7f8dfb113013ba91ad3
Atharv Bhadange <atharv1809>
2024-03-09 15:01:05 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
 import AppName from "./Components/AppName";
 import AppTodo from "./Components/AppTodo";
 import TodoItems from "./Components/TodoItems";
-import { useState } from "react";
+import { useReducer } from "react";
 import WelcomeMessage from "./Components/WelcomeMessage";
 import "./App.css";
+const todoItemsReducer=(currItems,action)=>
+{
+  let newItems=currItems;
+  if(action.type==='NEW_ITEM'){
+    newItems=[...currItems,{name: action.payload.itemName, date: action.payload.Date}];
+  }
+  else if(action.type==='DELETE_ITEM'){
+    newItems=currItems.filter((item)=>item.name!=action.payload.itemName);
+  }
+  return newItems;
+}
 function App()
 {
-  // let todoitems=[                            initial data
-  //   {name:"Buy milk", date:"3/10/2024"},
-  //   {name:"Go to college", date:"3/10/2024"},
-  // ];
-  const [items,setNewItems]=useState([]);
+  const [items,dispatchTodoItems]=useReducer(todoItemsReducer,[]);
   const handleAddButton=(itemName,itemDate)=>
   {
-    let newItems=[...items,{name: itemName, date: itemDate}];
-    setNewItems(newItems);
+    const newItemAction={
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        itemDate,
+      }
+    }
+    dispatchTodoItems(newItemAction);
   };
   const handleDeleteButton=(itemName)=>
   {
-    let newItems=items.filter((item)=>item.name!=itemName);
-    setNewItems(newItems);
+    const deleteItemAction={
+      type: "DELETE_ITEM",
+      payload: {
+        itemName,
+      }
+    }
+    dispatchTodoItems(deleteItemAction);
   }
   return <center className="todo-container">
     <AppName></AppName>
